6110ae114d7619c183f3b834eee1e9e9d6f8e227
Sultan Bashammakh <117147649+Sallot27@users.noreply.github.com>
2023-12-06 21:33:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
 const clientId = "a4b616e1ef60967";
-const defaultAlbumId = 'Jfni3';
+    const defaultAlbumId = 'Jfni3';
 
-function requestAlbum() {
-  let albumId = document.getElementById("albumIdField").innerText;
-  if (!albumId) {
-    albumId = defaultAlbumId;
-  }
+    function requestAlbum() {
+      let albumId = document.getElementById("albumIdField").value;
+      if (!albumId) {
+        albumId = defaultAlbumId;
+      }
 
-  fetch('https://api.imgur.com/3/album/' + albumId + '/images', {
-    headers: {
-      'Authorization': 'Client-ID ' + clientId
+      fetch('https://api.imgur.com/3/album/' + albumId + '/images', {
+        headers: {
+          'Authorization': 'Client-ID ' + clientId
+        }
+      })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Error with the Imgur API');
+        }
+      })
+      .then(data => {
+        data.data.slice(0, 10).forEach(item => {
+          console.log(item);
+          requestImage(item.id);
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
     }
-  })
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error('Error with the imgur API');
-    }
-  })
-  .then(data => {
-    data.slice(0, 10).forEach(item => {
-      console.log(item);
-      requestImage(item.id);
-    });
-  })
-  .catch(error => {
-    console.log(error);
-  });
-}
 
-function requestImage(imageHash) {
-  fetch('https://api.imgur.com/3/image/' + imageHash, {
-    headers: {
-      'Authorization': 'Client-ID ' + clientId
+    function requestImage(imageHash) {
+      fetch('https://api.imgur.com/3/image/' + imageHash, {
+        headers: {
+          'Authorization': 'Client-ID ' + clientId
+        }
+      })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Error with the Imgur API');
+        }
+      })
+      .then(data => {
+        const imgElem = document.createElement("img");
+        imgElem.src = data.data.link;
+        document.body.appendChild(imgElem);
+      })
+      .catch(error => {
+        console.log(error);
+      });
     }
-  })
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error('Error with the imgur API');
-    }
-  })
-  .then(data => {
-    const imgElem = document.createElement("img");
-    imgElem.src = data.data.link;
-    document.body.appendChild(imgElem);
-  })
-  .catch(error => {
-    console.log(error);
-  });
-}
